a4cc778250205b12eaa31c95008d145820916488
Clownier <Clownier.97@gmail.com>
2018-07-08 00:23:00 +0800

    delete init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,3 @@
 import xlrd
 import sys
-sys.path.append(".")
-from . import readFile
-path = r"./../DATA/jester-data-1.xls"
 if __name__ == "__main__":
-    date = readFile.readFile(path)
-    readFile.get_data(date)
